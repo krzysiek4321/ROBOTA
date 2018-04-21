04e13f4c6ea778a6f27280a0fbac8849fe989577
Krzysiek <multikys98@wp.pl>
2018-04-21 16:39:36 +0200

    Course,Subject,Test

--- src/main/java/com/example/demo/model/Test.java
+++ src/main/java/com/example/demo/model/Test.java
@@ -8,6 +8,22 @@ import java.time.LocalDate;
 
 @Entity
 public class Test {
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
